d5cb42efd45e46a05d7cd93a7b7b81c030bfdc31
orusovten <60890292+orusovten@users.noreply.github.com>
2022-02-07 15:38:14 +0300

    Create 0071a.cpp

---
+++ orusov_t_d/prj.codeforces/0071a/0071a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+void cut_to_abbreviation(const std::string& str) {
+    int len = str.size();
+    if (len <= 10) {
+        std::cout << str << std::endl;
+        return;
+    }
+    std::cout << str[0];
+    std::cout << len - 2;
+    std::cout << str[len - 1] << std::endl;
+}
+ 
+int main()
+{
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::string str;
+        std::cin >> str;
+        cut_to_abbreviation(str);
+    }
+}
