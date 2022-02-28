62c44495768ca668fc4a3d703da634bedd14574b
orusovten <60890292+orusovten@users.noreply.github.com>
2022-02-28 19:08:02 +0300

    Update m3i.cpp

--- orusov_t_d/prj.lab/m3i/m3i.cpp
+++ orusov_t_d/prj.lab/m3i/m3i.cpp
@@ -4,28 +4,31 @@ int min(int a, int b) {
     return a < b ? a : b;
 }
 
-void M3i::initialize() {
-    arr_ = new int**[d1_];
-    for (int i = 0; i < d1_; ++i) {
-        arr_[i] = new int*[d2_];
-        for (int j = 0; j < d2_; ++j) {
-            arr_[i][j] = new int[d3_];
-            for (int k = 0; k < d3_; ++k) {
-                arr_[i][j][k] = int();
+void M3i::initialize(int*** arr, int d1, int d2, int d3) {
+    arr = new int**[d1];
+    for (int i = 0; i < d1; ++i) {
+        arr[i] = new int*[d2];
+        for (int j = 0; j < d2; ++j) {
+            arr[i][j] = new int[d3];
+            for (int k = 0; k < d3; ++k) {
+                arr[i][j][k] = int();
             }
         }
     }
 }
 
-M3i::M3i(int d1, int d2, int d3): d1_(d1), d2_(d2), d3_(d3) {
-    initialize();
+M3i::M3i(int d1, int d2, int d3): 
+    d1_(new int(d1)), d2_(new int(d2)), d3_(new int(d3)), count_(new int(1))
+{
+    initialize(arr_, *d1_, *d2_, *d3_);
 }
 
 M3i::M3i(const il3int& list) {
-    d1_ = list.size(); 
-    d2_ = list.begin()->size();
-    d3_ = list.begin()->begin()->size();
-    initialize();
+    d1_ = new int(list.size());
+    d2_ = new int (list.begin()->size());
+    d3_ = new int(list.begin()->begin()->size());
+    count_ = new int(1);
+    initialize(arr_, *d1_, *d2_, *d3_);
     int i = 0;
     int j = 0;
     int k = 0;
@@ -44,8 +47,8 @@ M3i::M3i(const il3int& list) {
 }
 
 void M3i::clear() {
-    for (int i = 0; i < d1_; ++i) {
-        for (int j = 0; j < d2_; ++j) {
+    for (int i = 0; i < *d1_; ++i) {
+        for (int j = 0; j < *d2_; ++j) {
             delete[] arr_[i][j];
         }
         delete[] arr_[i];
@@ -53,9 +56,32 @@ void M3i::clear() {
     delete[] arr_;
 }
 
+M3i::M3i(const M3i& other): 
+    arr_(other.arr_), d1_(other.d1_), d2_(other.d2_), d3_(other.d3_), count_(other.count_)
+{
+    ++(*count_);
+}
+
+M3i& M3i::operator=(const M3i& other) {
+    if (this == &other) {
+        return *this;
+    }
+    arr_ = other.arr_;
+    d1_ = other.d1_;
+    d2_ = other.d2_;
+    d3_ = other.d3_;
+    count_ = other.count_;
+    ++(*count_);
+    return *this;
+}
+ 
 M3i::M3i(M3i&& other): 
-    arr_(other.arr_), d1_(other.d1_), d2_(other.d2_), d3_(other.d3_) {
+    arr_(other.arr_), d1_(other.d1_), d2_(other.d2_), d3_(other.d3_), count_(other.count_) {
     other.arr_ = nullptr;
+    other.d1_ = nullptr;
+    other.d2_ = nullptr;
+    other.d3_ = nullptr;
+    other.count_ = nullptr;
 }
 
 M3i& M3i::operator=(M3i&& other) {
@@ -65,6 +91,8 @@ M3i& M3i::operator=(M3i&& other) {
     clear();
     arr_ = other.arr_;
     other.arr_ = nullptr;
+    count_ = other.count_;
+    other.count_ = nullptr;
     d1_ = other.d1_;
     d2_ = other.d2_;
     d3_ = other.d3_;
@@ -72,7 +100,13 @@ M3i& M3i::operator=(M3i&& other) {
 }
 
 M3i::~M3i() {
-    clear();
+    if (--(*count_) == 0) {
+        clear();
+        delete d1_;
+        delete d2_;
+        delete d3_;
+        delete count_;
+    }
 }
 
 int& M3i::at(int i, int j, int k) {
@@ -84,10 +118,10 @@ int M3i::at(int i, int j, int k) const {
 }
 
 M3i M3i::clone() const {
-    M3i copy(d1_, d2_, d3_);
-    for (int i = 0; i < d1_; ++i) {
-        for (int j = 0; j < d2_; ++j) {
-            for (int k = 0; k < d3_; ++k) {
+    M3i copy(*d1_, *d2_, *d3_);
+    for (int i = 0; i < *d1_; ++i) {
+        for (int j = 0; j < *d2_; ++j) {
+            for (int k = 0; k < *d3_; ++k) {
                 copy.at(i, j, k) = arr_[i][j][k];
             }
         }
@@ -96,37 +130,39 @@ M3i M3i::clone() const {
 }
 
 M3i& M3i::resize(int d1, int d2, int d3) {
-    int*** old_arr = arr_;
-    int old_d1 = d1_;
-    int old_d2 = d2_;
-    int old_d3 = d3_;
-    d1_ = d1;
-    d2_ = d2;
-    d3_ = d3;
-    initialize();
-    for (int i = 0; i < min(d1_, old_d1); ++i) {
-        for (int j = 0; j < min(d2_, old_d2); ++j) {
-            for (int k = 0; k < min(d3_, old_d3); ++k) {              
-                arr_[i][j][k] = old_arr[i][j][k];
+    int*** new_arr;
+    initialize(new_arr, d1, d2, d3);
+    for (int i = 0; i < min(*d1_, d1); ++i) {
+        for (int j = 0; j < min(*d2_, d2); ++j) {
+            for (int k = 0; k < min(*d3_, d3); ++k) {              
+                new_arr[i][j][k] = arr_[i][j][k];
             }
         }
     }
+    clear();
+    arr_ = new_arr;
+    delete d1_;
+    delete d2_;
+    delete d3_;
+    d1_ = new int(d1);
+    d2_ = new int(d2);
+    d3_ = new int(d3);
     return *this;
 }
 
 int M3i::size(int dim) const {
     if (dim == 0) {
-        return d1_;
+        return *d1_;
     } else if (dim == 1) {
-        return d2_;
+        return *d2_;
     }
-    return d3_;
+    return *d3_;
 }
 
 void M3i::fill(int val) {
-    for(int i = 0; i < d1_; ++i) {
-        for(int j = 0; j < d2_; ++j) {
-            for (int k = 0; k < d3_; ++k) {
+    for(int i = 0; i < *d1_; ++i) {
+        for(int j = 0; j < *d2_; ++j) {
+            for (int k = 0; k < *d3_; ++k) {
                 arr_[i][j][k] == val;
             }
         }
