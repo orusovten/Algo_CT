d90437126d80090e888b75368677c49f7141be47
orusovten <60890292+orusovten@users.noreply.github.com>
2022-03-06 22:55:42 +0300

    Update m3i.h

--- orusov_t_d/prj.lab/m3i/m3i.h
+++ orusov_t_d/prj.lab/m3i/m3i.h
@@ -1,45 +1,103 @@
+#pragma once
+#ifndef M3i_HEAD_H_2022_03_03
+#define M3i_HEAD_H_2022_03_03
+
 #include <iostream>
-#include <initializer_list>
+#include <memory>
+#include <Initializer_list>
+#include <mutex>
 typedef std::initializer_list<std::initializer_list<std::initializer_list<int>>> il3int;
 
+namespace details {
+    class M3iImpl {
+    public:
+        M3iImpl(); // done
+        M3iImpl(int d1, int d2, int d3); // done
+        // мой вариант конструктора от std::Initializer_list
+        M3iImpl(const il3int& list); // done
+        // легкая копия(подсчет ссылок)
+        M3iImpl(const M3iImpl& other); // done
+        // легкая копия(подсчет ссылок)
+        M3iImpl& operator=(const M3iImpl& other); // done
+
+        M3iImpl(M3iImpl&& other); // done
+        M3iImpl& operator=(M3iImpl&& other); // done
+
+        ~M3iImpl(); // done
+        
+        // полная копия данных
+        M3iImpl Clone() const; // done
+
+        M3iImpl& Resize(int d1, int d2, int d3); // done
+
+        // доступ к элементу
+        int& At(int i, int j, int k); // done
+        int At(int i, int j, int k) const; // done
+
+        // получение размера по измерению 0, 1, 2
+        int Size(int dim) const; // done
+        // заполнение значениями
+        void Fill(int val); // done
+    private:
+        void Initialize(int***& arr, int d1, int d2, int d3); // заполнение arr значениями по умолчанию
+        void Clear(); // очистка данных
+        struct SharedPtr {
+            int*** arr; // буфер
+            int d1; // измерение 1
+            int d2; // измерение 2
+            int d3; // измерение 3
+            int count; // кол-во ссылок
+            SharedPtr(int*** arr, int d1, int d2, int d3, int count): 
+                arr(arr), d1(d1), d2(d2), d3(d3), count(count)
+            {}
+        };
+        SharedPtr* ptr_;
+    };
+}
+
+std::istream& operator>>(std::istream& istrm, details::M3iImpl& m);
+std::ostream& operator<<(std::ostream& ostrm, const details::M3iImpl& m); // done
+
 class M3i {
+private:
+    // using namespace details;
 public:
     M3i() = default; // done
     M3i(int d1, int d2, int d3); // done
-    // мой вариант конструктора от std::initializer_list
+    // мой вариант конструктора от std::Initializer_list
     M3i(const il3int& list); // done
     // легкая копия(подсчет ссылок)
-    M3i(const M3i& other); // done, but questions
+    M3i(const M3i& other); // done
     // легкая копия(подсчет ссылок)
-    M3i& operator=(const M3i& other); // done, but questions
+    M3i& operator=(const M3i& other); // done
 
-    M3i(M3i&& other); // done, but questions
-    M3i& operator=(M3i&& other); // done, but questions
+    M3i(M3i&& other); // done
+    M3i& operator=(M3i&& other); // done
 
-    ~M3i(); // done
+    ~M3i() = default; // done
     
     // полная копия данных
-    M3i clone() const; // done
+    M3i Clone() const; // done
 
-    M3i& resize(int d1, int d2, int d3); // done
+    M3i& Resize(int d1, int d2, int d3); // done
 
     // доступ к элементу
-    int& at(int i, int j, int k); // done
-    int at(int i, int j, int k) const; // done
+    int& At(int i, int j, int k); // done
+    int At(int i, int j, int k) const; // done
 
     // получение размера по измерению 0, 1, 2
-    int size(int dim) const; // done
+    int Size(int dim) const; // done
     // заполнение значениями
-    void fill(int val); // done
+    void Fill(int val); // done
+
+    std::ostream& WriteTo(std::ostream& ostrm) const; // done
 private:
-    void initialize(int*** arr, int d1, int d2, int d3); // заполнение arr значениями по умолчанию
-    void clear(); // очистка данных
-    int*** arr_; // буфер
-    int* d1_; // измерение 1
-    int* d2_; // измерение 2
-    int* d3_; // измерение 3
-    int* count_; // кол-во ссылок
+    explicit M3i(details::M3iImpl impl);
+    details::M3iImpl impl_;
+    std::shared_ptr<std::mutex> mutex_ = std::make_shared<std::mutex>();
 };
 
 std::istream& operator>>(std::istream& istrm, M3i& m);
 std::ostream& operator<<(std::ostream& ostrm, const M3i& m); // done
+
+#endif // #define M3i_HEAD_H_2022_03_03
