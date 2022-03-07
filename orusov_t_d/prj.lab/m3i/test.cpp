005e3b3bef14f467ea0217ebf73b96d7555204d1
orusovten <60890292+orusovten@users.noreply.github.com>
2022-03-07 22:25:26 +0300

    Update test.cpp

--- orusov_t_d/prj.lab/m3i/test.cpp
+++ orusov_t_d/prj.lab/m3i/test.cpp
@@ -1,10 +1,28 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <string>
-#include <doctest/doctest.h>
+#include <doctest/doctest.h.
 #include <m3i/m3i.h>
+#include <sstream>
 
 using namespace details;
+
+bool operator== (const M3i& left, const M3i& right) {
+    if ((left.Size(0) != right.Size(0)) || (left.Size(1) != right.Size(1)) || 
+                                            (left.Size(2) != right.Size(2))) {
+        return false;
+    }
+    for(int i = 0; i < left.Size(0); ++i) {
+        for(int j = 0; j < left.Size(1); ++j) {
+            for(int k = 0; k < left.Size(2); ++k) {
+                if (left.At(i, j, k) != right.At(i, j, k)) {
+                    return false;
+                }                
+            }
+        }
+    }
+    return true;
+}
 
 TEST_CASE("init")
 {
@@ -33,21 +51,9 @@ TEST_CASE("init")
         int d3 = 4;
         M3i m3i(d1, d2, d3);
         M3i copy_m3i(m3i);
-        for(int i = 0; i < d1; ++i) {
-            for(int j = 0; j < d2; ++j) {
-                for(int k = 0; k < d3; ++k) {
-                    CHECK(m3i.At(i, j, k) == copy_m3i.At(i, j, k));
-                }
-            }
-        }
+        CHECK(copy_m3i == m3i);
         copy_m3i = m3i;
-        for(int i = 0; i < d1; ++i) {
-            for(int j = 0; j < d2; ++j) {
-                for(int k = 0; k < d3; ++k) {
-                    CHECK(m3i.At(i, j, k) == copy_m3i.At(i, j, k));
-                }
-            }
-        }
+        CHECK(copy_m3i == m3i);
     }
     SUBCASE("move")
     {
@@ -57,22 +63,10 @@ TEST_CASE("init")
         M3i m3i(d1, d2, d3);
         M3i copy_m3i = m3i;
         M3i move_m3i(std::move(m3i));
-        for(int i = 0; i < d1; ++i) {
-            for(int j = 0; j < d2; ++j) {
-                for(int k = 0; k < d3; ++k) {
-                    CHECK(move_m3i.At(i, j, k) == copy_m3i.At(i, j, k));
-                }
-            }
-        }
+        CHECK(move_m3i == copy_m3i);
         M3i second_m3i(d1, d2, d3);
         move_m3i = std::move(second_m3i);
-        for(int i = 0; i < d1; ++i) {
-            for(int j = 0; j < d2; ++j) {
-                for(int k = 0; k < d3; ++k) {
-                    CHECK(move_m3i.At(i, j, k) == copy_m3i.At(i, j, k));
-                }
-            }
-        }
+        CHECK(move_m3i == copy_m3i);
     }
     SUBCASE("init by list")
     {
@@ -107,16 +101,7 @@ TEST_CASE("resize")
             }
         }
     }
-    CHECK(copy.Size(0) == 3);
-    CHECK(copy.Size(1) == 1);
-    CHECK(copy.Size(2) == 5);
-    for (int i = 0; i < m3i.Size(0); ++i) {
-        for (int j = 0; j < m3i.Size(1); ++j) {
-            for (int k = 0; k < m3i.Size(2); ++k) {
-                CHECK(m3i.At(i, j, k) == copy.At(i, j, k));
-            }
-        }
-    }
+    CHECK(copy == m3i);
 }
 
 TEST_CASE("fill")
@@ -127,8 +112,40 @@ TEST_CASE("fill")
     for (int i = 0; i < m3i.Size(0); ++i) {
         for (int j = 0; j < m3i.Size(1); ++j) {
             for (int k = 0; k < m3i.Size(2); ++k) {
-                CHECK(m3i.At(i, j, k) == copy.At(i, j, k));
                 CHECK(m3i.At(i, j, k) == 4);
+            }
+        }
+    }
+    CHECK(copy == m3i);
+}
+
+TEST_CASE("input/output") {
+    M3i m3i;
+    std::stringstream input_stream;
+    input_stream << "2 2 3\n 1 2 3\n 4 5 6\n 7 8 9\n 10 11 12\n";
+    input_stream >> m3i;
+    M3i right_m3i = {{{1, 2, 3}, {4, 5, 6}}, {{7, 8, 9}, {10, 11, 12}}};
+    CHECK(m3i == right_m3i);
+    std::stringstream output_stream;
+    output_stream << m3i;
+    std::string str_m3i;
+    output_stream >> str_m3i;
+    CHECK(str_m3i == "size:");
+    int d1;
+    int d2;
+    int d3;
+    output_stream >> d1;
+    output_stream >> d2;
+    output_stream >> d3;
+    CHECK(d1 == 2);
+    CHECK(d2 == 2);
+    CHECK(d3 == 3);
+    for(int i = 0; i < d1; ++i) {
+        for(int j = 0; j < d2; ++j) {
+            for(int k = 0; k < d3; ++k) {
+                int number;
+                output_stream >> number;
+                CHECK(right_m3i.At(i, j, k) == number);    
             }
         }
     }
