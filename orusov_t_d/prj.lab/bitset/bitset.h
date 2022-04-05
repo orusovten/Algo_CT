9317006634a916d7d146f30c379871860c332187
orusovten <orusov22112001@mail.ru>
2022-04-05 05:05:41 +0300

    update

--- orusov_t_d/prj.lab/bitset/bitset.h
+++ orusov_t_d/prj.lab/bitset/bitset.h
@@ -1,30 +1,51 @@
 #pragma once
-#ifndef BITSET_HEAD_H_2022_03_03
-#define BITSET_HEAD_H_2022_03_03
+#ifndef BITSET_HEAD_H_2022_03_29
+#define BITSET_HEAD_H_2022_03_29
+#include <vector>
+#include <cstdint>
+#include <iosfwd>
 
-class Bitset {
+class BitSet {
+private:
+    // класс BoolHolder - деталь реализации
+    class BoolHolder;
 public:
-    Bitset();
-    Bitset(const Bitset& other);
-    Bitset(Bitset&& other);
-    ~Bitset();
-    Bitset& operator=(const Bitset& other);
-    Bitset& operator=(Bitset&& other);
-    Bitset(const int size, const bool val = false);
+    BitSet() = default;
+    BitSet(const BitSet& other) = default;
+    BitSet(BitSet&& other) = default;
+    ~BitSet() = default;
+    BitSet& operator=(const BitSet& other) = default;
+    BitSet& operator=(BitSet&& other) = default;
+    BitSet(const int size, const bool val = false);
     int Size() const;
     void Resize(const int size);
     bool operator[](const int index) const;
-    Bitset& operator|(const Bitset& other);
-    Bitset& operator&(const Bitset& other);
-    Bitset& operator^(const Bitset& other);
-    Bitset& operator~();
+    BoolHolder operator[](const int index);
+    BitSet& operator|=(const BitSet& other);
+    BitSet& operator&=(const BitSet& other);
+    BitSet& operator^=(const BitSet& other);
+    BitSet operator~() const;
+
+    // метод At - деталь реализации
+    bool At(const int index) const; 
+    // BoolHolder At(const int index);
 private:
+    // класс BoolHolder - деталь реализации
     class BoolHolder {
         BoolHolder& operator=(const BoolHolder& other);
         private:
             BoolHolder();
             unsigned char* ptr_;
     };
+    std::vector<uint16_t> array_; 
+    int size_ = 0;
+    static const int BIT_SIZE = 16;
 };
 
-#endif // #define BITSET_HEAD_H_2022_03_03
+const BitSet operator^ (const BitSet& left, const BitSet& right);
+const BitSet operator& (const BitSet& left, const BitSet& right);
+const BitSet operator| (const BitSet& left, const BitSet& right);
+
+std::ostream& operator<<(std::ostream& ostrm, const BitSet& bs);
+
+#endif // #define BITSET_HEAD_H_2022_03_29
