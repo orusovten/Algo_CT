5055f780cf2df766f4d74559c62f197632ad5eef
orusovten <orusov22112001@mail.ru>
2022-04-10 02:18:13 +0300

    add exceptions

--- orusov_t_d/prj.lab/bitset/test.cpp
+++ orusov_t_d/prj.lab/bitset/test.cpp
@@ -9,7 +9,7 @@ bool operator==(const BitSet& left, const BitSet& right) {
         return false;
     }
     for (int i = 0; i < left.Size(); ++i) {
-        if (left.At(i) != right.At(i)) {
+        if (left[i] != right[i]) {
             return false;
         }
     }
@@ -20,12 +20,16 @@ TEST_CASE("constructor") {
     SUBCASE("simple init") {
         BitSet bitset();
     }
+    SUBCASE("bad init") {
+        CHECK_THROWS(BitSet(-1, true));
+        CHECK_THROWS(BitSet(0, true));
+    }
     SUBCASE("simple init with size") {
         int size = 40;
         BitSet bitset(size, true);
         CHECK(bitset.Size() == size);
         for (int i = 0; i < bitset.Size(); ++i) {
-            CHECK(bitset.At(i));
+            CHECK(bitset[i]);
         }
     }
     SUBCASE("copy constructor") {
@@ -68,10 +72,10 @@ TEST_CASE("resize") {
         bitset.Resize(new_size);
         CHECK(bitset.Size() == new_size);
         for (int i = 0; i < size; ++i) {
-            CHECK(bitset.At(i));
+            CHECK(bitset[i]);
         }
         for (int i = size; i < new_size; ++i) {
-            CHECK_FALSE(bitset.At(i));
+            CHECK_FALSE(bitset[i]);
         }
     }
     SUBCASE("resize to smaller") {
@@ -81,9 +85,26 @@ TEST_CASE("resize") {
         bitset.Resize(new_size);
         CHECK(bitset.Size() == new_size);
         for (int i = 0; i < new_size; ++i) {
-            CHECK(bitset.At(i));
+            CHECK(bitset[i]);
         }
     }
+    SUBCASE("bad resize") {
+        int size = 40;
+        BitSet bitset(size, true);
+        CHECK_THROWS(bitset.Resize(0));
+        CHECK_THROWS(bitset.Resize(-1));
+    }
+}
+
+TEST_CASE("fill") {
+    int size = 40;
+    BitSet positive_bitset(size, true);
+    BitSet bitset = positive_bitset;
+    BitSet minus_bitset(size, false);
+    bitset.Fill(false);
+    CHECK(minus_bitset == bitset);
+    bitset.Fill(true);
+    CHECK(positive_bitset == bitset);
 }
 
 TEST_CASE("operators") {
@@ -93,7 +114,7 @@ TEST_CASE("operators") {
         auto minus_bitset = ~bitset;
         CHECK(minus_bitset.Size() == size);
         for (int i = 0; i < size; ++i) {
-            CHECK_FALSE(minus_bitset.At(i));
+            CHECK_FALSE(minus_bitset[i]);
         }
         auto plus_bitset = ~minus_bitset;
         CHECK(plus_bitset.Size() == size);
@@ -101,4 +122,90 @@ TEST_CASE("operators") {
             CHECK(plus_bitset == bitset);
         }
     }
-}
+    SUBCASE("operator[] BitHolder") {
+        int size = 40;
+        BitSet bitset(size, true);
+        for (int i = 0; i < size; ++i) {
+            bitset[i] = false;
+        }
+        BitSet correct_bitset(size, false);
+        CHECK(bitset == correct_bitset);
+        CHECK_THROWS(bitset[-1]);
+        CHECK_THROWS(bitset[size]);
+    }
+    SUBCASE("simple operators & and &=") {
+        int size = 40;
+        BitSet first(size, true);
+        BitSet wrong_second(size + 1, true);
+        CHECK_THROWS(first & wrong_second);
+        CHECK_THROWS(first &= wrong_second);
+        BitSet second(size, true);
+        first[33] = 0;
+        first[20] = 0;
+        first[30] = 0;
+        first[15] = 0;
+        second[20] = 0;
+        second[1] = 0;
+        second[33] = 0;
+        second[4] = 0;
+        second[36] = 0;
+        BitSet result(size, true);
+        for (auto index : {1, 4, 15, 20, 30, 33, 36}) {
+            result[index] = false;
+        }
+        auto res = first & second;
+        CHECK(res == result);
+        first &= second;
+        CHECK(first == result);
+    }
+    SUBCASE("simple operators | and |=") {
+        int size = 40;
+        BitSet first(size, true);
+        BitSet wrong_second(size + 1, true);
+        CHECK_THROWS(first | wrong_second);
+        CHECK_THROWS(first |= wrong_second);
+        BitSet second(size, true);
+        first[33] = 0;
+        first[20] = 0;
+        first[30] = 0;
+        first[15] = 0;
+        second[20] = 0;
+        second[1] = 0;
+        second[33] = 0;
+        second[4] = 0;
+        second[36] = 0;
+        BitSet result(size, true);
+        for (auto index : {20, 33}) {
+            result[index] = false;
+        }
+        auto res = first | second;
+        CHECK(res == result);
+        first |= second;
+        CHECK(first == result);
+    }
+    SUBCASE("simple operators ^ and ^=") {
+        int size = 40;
+        BitSet first(size, true);
+        BitSet wrong_second(size + 1, true);
+        CHECK_THROWS(first ^ wrong_second);
+        CHECK_THROWS(first ^= wrong_second);
+        BitSet second(size, true);
+        first[33] = 0;
+        first[20] = 0;
+        first[30] = 0;
+        first[15] = 0;
+        second[20] = 0;
+        second[1] = 0;
+        second[33] = 0;
+        second[4] = 0;
+        second[36] = 0;
+        BitSet result(size, false);
+        for (auto index : {1, 4, 15, 30, 36}) {
+            result[index] = true;
+        }
+        auto res = first ^ second;
+        CHECK(res == result);
+        first ^= second;
+        CHECK(first == result);
+    }
+}   
